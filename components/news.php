67885942aba085ade48cac91ac14a367560b8bdc
sonys03 <80260565+sonys03@users.noreply.github.com>
2021-07-12 18:12:49 +0300

    Add files via upload

---
+++ components/news.php
@@ -0,0 +1,10 @@
+<section class="news container">
+  <h2 class="section-title">News</h2>
+  <article class="card">
+    <img src="img/hbr.svg" width="56" height="56" alt="HTML">
+    <div class="card-content">
+      <h3>Все ок</h3>
+      <time datetime="2021-07-12">12 июля 2021</time>
+    </div>
+  </article>
+</section>
---
+++ components/quotes.php
@@ -0,0 +1,11 @@
+<section class="quotes container">
+  <h2 class="section-title">цитаты</h2>
+  <blockquote>
+    <p>Я уже у нее в голове</p>
+    <cite>Конфуций</cite>
+  </blockquote>
+  <blockquote>
+    <p>Наелся и спит</p>
+    <cite>Аристотель</cite>
+  </blockquote>
+</section>
